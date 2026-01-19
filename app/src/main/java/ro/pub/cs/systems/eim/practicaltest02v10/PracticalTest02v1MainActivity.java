f342e071550a5a32d18e726128633fcb7802841d
vlads <vlad.stanciu@qualitestgroup.com>
2026-01-19 08:33:37 +0200

    grafica si thread uri client server

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v10/PracticalTest02v1MainActivity.java
@@ -0,0 +1,212 @@
+package ro.pub.cs.systems.eim.practicaltest02v10;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class PracticalTest02v1MainActivity extends AppCompatActivity {
+    private final String hostname = "10.0.2.15";
+    private EditText port;
+
+    private Button btnStartServer;
+
+    private Button btnConnectClient;
+
+    private TextView textView;
+
+    private OkHttpClient okHttpClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_practical_test02v1_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        port = findViewById(R.id.port);
+
+        btnStartServer = findViewById(R.id.btnStartServer);
+
+        btnConnectClient = findViewById(R.id.btnConnectClient);
+
+        textView = findViewById(R.id.textView);
+
+        btnStartServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerThread serverThread = new ServerThread();
+                serverThread.startServer();
+            }
+        });
+
+        btnConnectClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Runnable runnable = new ThreadClient(hostname,Integer.parseInt(port.getText().toString()));
+                new Thread(runnable).start();
+            }
+        });
+
+    }
+
+    private class HttpThread implements Runnable {
+        @Override
+        public void run() {
+            okHttpClient = new OkHttpClient();
+            String url = "https://www.google.com/complete/search?client=chrome&q=" + textView.getText().toString();
+            Request request = new Request.Builder().url(url).build();
+            try {
+                Response response = okHttpClient.newCall(request).execute();
+                if(response.isSuccessful() && response.body()!=null) {
+                    String jsonResponse = response.body().string();
+                    JSONArray jsonObject = new JSONArray(jsonResponse);
+                    var values = jsonObject.getJSONArray(1);
+                    Log.d("TAG", jsonObject.toString());
+                    Log.d("TAG",jsonResponse);
+                    Log.d("TAG",values.toString());
+                    StringBuilder result = new StringBuilder("");
+                    for(int i = 0 ; i < values.length(); i++) {
+                        Log.d("TAG",values.getString(i));
+                        result.append( i + ") " + values.getString(i) + "\n");
+                    }
+                    Intent intent = new Intent("AUTOCOMPLETE_RESULTS");
+                    intent.setPackage(getPackageName());
+                    intent.putExtra("resultsAutocomplete", result.toString());
+                    sendBroadcast(intent); // din Activity (inner class) merge
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            resultText.setText(result.toString());
+//                        }
+//                    });
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    class ServerThread extends Thread implements Runnable {
+        private boolean serverRunning;
+        private ServerSocket serverSocket;
+
+        private int count = 0;
+
+        public void startServer() {
+            if(!serverRunning) {
+                serverRunning = true;
+                start();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(Integer.parseInt(port.getText().toString()));
+
+                while(serverRunning) {
+                    Socket socket = serverSocket.accept();
+                    count++;
+                    Log.d("TAG","Client s-a conectat");
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            textView.setText("Client " + count + " connected with address : " + socket.getInetAddress());
+//                        }
+//                    });
+                    ;
+
+                    PrintWriter output_Server = new PrintWriter(socket.getOutputStream());
+                    output_Server.write("Welcome to Server: " + count);
+                    output_Server.flush();
+
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void stopServer() {
+            serverRunning=false;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if(serverSocket!=null) {
+                        try {
+                            serverSocket.close();
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    textView.setText("Server stopped");
+//                                }
+//                            });
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+    private class ThreadClient implements  Runnable {
+        private String hostname;
+        private int port;
+
+        public ThreadClient(String hostname, int port) {
+            this.hostname = hostname;
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket(this.hostname, this.port);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line = bufferedReader.readLine();
+                socket.close();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView = findViewById(R.id.textView);
+                        textView.setText("Connected! The message is : " + line);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
